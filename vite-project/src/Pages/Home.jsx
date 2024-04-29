dcf532c8a2d6c3188ce5adf8e508f1ae2eb81400
Tanya Saxena <87142261+tanya2468@users.noreply.github.com>
2024-04-29 23:19:42 +0530

    updated cors

---
+++ vite-project/src/Pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Navbar from '../Components/Navbar'
+import '../assets/css/home.css' 
+const Home = () => {
+  return (
+    <>
+    <Navbar />
+    <div className='home'>
+      <h1 className='home-title'>
+        CONTACT SPHERE
+      </h1>
+      <p className='home-description'>
+            Collect your contacts in a smarter way.
+      </p>
+    </div>
+    </>
+  )
+}
+
+export default Home
